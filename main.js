58a36c73764eb077999a4a89299d1c0e2127c582
Julien Noble <penguzine@gmail.com>
2014-09-17 23:12:02 +0200

    testing text

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
- 
-function preload() {
-}
- 
-function create() {
-}
- 
-function update() {
-}
+// your saviour : http://docs.phaser.io
+
+var main = {
+	preload: function() {
+		// lol you lost
+	},
+
+	create: function() { 
+		this.gametext = game.add.text(400, 300, 'The Game', { fontSize: '72px', fill: '#fff' });
+	},
+
+	update: function() {
+		
+	},
+};
+
+// Initialize Phaser, and start our 'main' state 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'thegame');
+game.state.add('main', main);
+game.state.start('main');
+
