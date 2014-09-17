c20cae4fdfabc88c3c047ef14424854d5d8ef8ae
Julien Noble <penguzine@gmail.com>
2014-09-17 23:33:37 +0200

    oups2

--- main.js
+++ main.js
@@ -6,13 +6,22 @@ var main = {
 	},
 
 	create: function() { 
-		this.gametext = game.add.text(400, 300, 'The Game', { fontSize: '72px', fill: '#fff' });
 		this.circle = 0;
+		this.bmd = game.add.bitmapData(game.width, game.height);
+		this.bmd.fill(0, 0, 0, 1);
+		bmd.addToWorld();
+
+	    this.text = game.make.text(400, 300, "The Game", { font: "bold 32px Arial", fill: "#fff" });
+	    this.text.anchor.set(0.5);
 	},
 
 	update: function() {
-		this.gametext.world.x = 400 + Math.cos(this.circle) * 100;
-		this.gametext.world.y = 300 + Math.sin(this.circle) * 100;
+		this.bmd.fill(0, 0, 0, 0.05);
+
+		//	Un-comment to see the rotation in action
+		// text.rotation += 0.05;
+
+	    this.bmd.draw(this.text, 400 + Math.cos(this.circle)*100, 300 + Math.sin(this.circle)*100, null, null, 'destination-out');
 		this.circle += 0.1;
 	},
 };
